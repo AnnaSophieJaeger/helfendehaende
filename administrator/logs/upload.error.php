697c57727bcc8b592b8b7c639637ea499525c1d2
AnnaSophieJaeger <anna-sophie.jaeger@gmx.at>
2017-03-01 14:38:16 +0100

    Article für Reiter sind fertig

---
+++ administrator/logs/upload.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-02-03 13:18:59 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-02-03T13:18:59+00:00	INFO ::1	upload	
